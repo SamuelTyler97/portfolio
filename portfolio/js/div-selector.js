7708c6635d56017984cc31f276adacf430ff2bec
SamuelTyler97 <101990795+SamuelTyler97@users.noreply.github.com>
2024-04-18 14:11:56 -0400

    Add files via upload
    Improvements to div-selector script

--- portfolio/js/div-selector.js
+++ portfolio/js/div-selector.js
@@ -2,10 +2,26 @@ function showDiv(id) {
     // Hide all divs except for default
     var divs = document.querySelectorAll('#contentDiv > div');
     divs.forEach(function(div) {
-      div.classList.add('hidden');
+        div.classList.add('hidden');
     });
-    
+  
     // Show the selected div when link is clicked
     var selectedDiv = document.getElementById(id);
     selectedDiv.classList.remove('hidden');
+  
+    // Animate the image and paragraph text appearing
+    var animatedImage = selectedDiv.querySelector('.animated-image');
+    var paragraph = selectedDiv.querySelector('.left-border');
+    if (animatedImage) {
+        animatedImage.classList.add('fade-in-img');
+    }
+    if (paragraph) {
+        paragraph.classList.add('slide-in');
+    }
+  
+    // Smooth scroll to the main-logo element
+    document.getElementById('main-logo').scrollIntoView({
+      behavior: 'smooth'
+    });
   }
+  
